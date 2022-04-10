859964d926e1afffb6726904c179d144005cbc5b
lan <lanlehoang10@gmail.com>
2022-04-10 17:52:59 +0700

    justified right

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,28 +1,29 @@
-function print_calendar(month, year){
-    print_header(month, year);
+function printCalendar(month, year){
+    printHeader(month, year);
     const first_day_of_month = new Date(year, month -1, 1);
     const first_wday = first_day_of_month.getDay();
     const last_day_of_month = new Date(year, month, 0);
     const dates = getDates(first_day_of_month, last_day_of_month);
-    let str =  '   '.repeat(first_wday) + (first_wday>2?' '.repeat(first_wday-2):'')
+    let str =  '   '.repeat(first_wday) + (first_wday > 2 ? ' '.repeat(first_wday - 2) : '')
     dates.forEach(day => {
-        if(day.getDay() != 0 && day != first_day_of_month){
+        if(day.getDay() !== 0 && day !== first_day_of_month){
             str += '  ';
         }
         if(day.getDate() < 10){
-          str += day.getDate().toString();
-          str += ' '
+          str += ' ' + day.getDate().toString();
         }
-        else str += day.getDate().toString();
-        if(day.getDay() == 6){
+        else {
+          str +=  day.getDate().toString();
+        }
+        if(day.getDay() === 6){
             str += "\n";
         }
     })
     console.log(str)
 }
 
-function print_header(month, year){
-    console.log(`${month}月 ${year}年`);
+function printHeader(month, year){
+    console.log(`        ${month}月 ${year}年`);
     console.log('日  月  火  水  木  金  土');
 }
 
@@ -39,4 +40,4 @@ const argv = require('minimist')(process.argv.slice(2));
 const month = argv.m || now.month() + 1;
 const year = argv.y || now.year();
 
-print_calendar(month, year)
+printCalendar(month, year)
