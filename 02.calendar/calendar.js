86af5039e6cc7f9e9f91b482537625705263b828
lan <lanlehoang10@gmail.com>
2022-04-17 19:43:17 +0700

    fix indent, camelcase

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,28 +1,27 @@
-function printCalendar(month, year){
+function printCalendar(month, year) {
     printHeader(month, year);
-    const first_day_of_month = new Date(year, month -1, 1);
-    const first_wday = first_day_of_month.getDay();
-    const last_day_of_month = new Date(year, month, 0);
-    const dates = getDates(first_day_of_month, last_day_of_month);
-    let str =  '   '.repeat(first_wday) + (first_wday > 2 ? ' '.repeat(first_wday - 2) : '')
+    const firstDayOfMonth = new Date(year, month - 1, 1);
+    const firstWday = firstDayOfMonth.getDay();
+    const lastDayOfMonth = new Date(year, month, 0);
+    const dates = getDates(firstDayOfMonth, lastDayOfMonth);
+    let str = '   '.repeat(firstWday) + (firstWday > 2 ? ' '.repeat(firstWday - 2) : '')
     dates.forEach(day => {
-        if(day.getDay() !== 0 && day !== first_day_of_month){
+        if (day.getDay() !== 0 && day !== firstDayOfMonth) {
             str += '  ';
         }
-        if(day.getDate() < 10){
-          str += ' ' + day.getDate().toString();
+        if (day.getDate() < 10) {
+            str += ' ' + day.getDate().toString();
+        } else {
+            str += day.getDate().toString();
         }
-        else {
-          str +=  day.getDate().toString();
-        }
-        if(day.getDay() === 6){
+        if (day.getDay() === 6) {
             str += "\n";
         }
     })
     console.log(str)
 }
 
-function printHeader(month, year){
+function printHeader(month, year) {
     console.log(`        ${month}月 ${year}年`);
     console.log('日  月  火  水  木  金  土');
 }
@@ -31,7 +30,9 @@ function getDates(startDate, endDate) {
     const duration = endDate - startDate;
     const interval = 1000 * 60 * 60 * 24;
     const steps = duration / interval;
-    return Array.from({length: steps+1}, (v, i) => new Date(startDate.valueOf() + (interval * i)));
+    return Array.from({
+        length: steps + 1
+    }, (v, i) => new Date(startDate.valueOf() + (interval * i)));
 }
 
 const dayjs = require('dayjs')
